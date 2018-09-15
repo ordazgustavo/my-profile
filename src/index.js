fc35468422856583a64cdd85ca199eeb10c21415
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-09-15 01:48:21 -0500

    add local fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import 'typeface-ubuntu'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
