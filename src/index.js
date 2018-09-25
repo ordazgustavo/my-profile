9bdcdd3c2207745065449f5bf60524d8a647d811
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-09-25 00:20:39 -0500

    global styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'typeface-ubuntu'
-import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './globalStyles'
