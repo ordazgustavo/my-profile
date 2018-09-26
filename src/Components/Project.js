70e2a575d8b6c70c4ba58310457a33362549bdd6
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-09-25 19:29:37 -0500

    adding new projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default ({ name, description, link, label }) => (
+export default ({ name, description, tech, link, label }) => (
   <div>
     <p>
       <strong>{name}</strong>
     </p>
     <p>{description}</p>
+    <p>
+      <small>{tech}</small>
+    </p>
     <a href={link}>{label}</a>
   </div>
 )
--- src/personal/projects.js
+++ src/personal/projects.js
@@ -3,8 +3,25 @@ import uuid from 'uuid'
 export default [
   {
     id: uuid(),
-    name: 'Reflex',
+    name: 'Create React Project 📚',
+    description: 'Heavily opinionated React project boilerplate.',
+    tech: 'React',
+    link: 'https://github.com/ordazgustavo/create-react-project',
+    label: 'GitHub'
+  },
+  {
+    id: uuid(),
+    name: 'Blog personal 👨‍💻',
+    description: 'Mi blog personal, enfocado en desarrollo web.',
+    tech: 'React, Gatsby, GraphQL, Netlify CMS',
+    link: 'https://github.com/ordazgustavo/create-react-project',
+    label: 'Blog'
+  },
+  {
+    id: uuid(),
+    name: 'Reflex ⚛️',
     description: "JavaScript library to create dynamic UI's",
+    tech: 'Javascript',
     link: 'https://github.com/ordazgustavo/reflex',
     label: 'GitHub'
   },
@@ -13,6 +30,7 @@ export default [
     name: 'Soy Savio',
     description: `Autor de Soy Savio, aplicación web desarrollada en React, 
     que permite conectar a personas con agencias de cambio de manera segura.`,
+    tech: 'React, Firebase, IBM Cloud',
     link: 'http://soysavio.com',
     label: 'Visitar'
   }
